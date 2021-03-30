7339b45014031c5f41ccb7d649bbf6896bf85845
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2021-03-30 12:48:07 +0200

    Added styling

--- Lab1/Calculator.js
+++ Lab1/Calculator.js
@@ -1,48 +1,67 @@
 import React, {useState} from 'react'
-import { Button, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Button, Text, View } from 'react-native'
 import NumericInput from 'rn-numeric-input'
 
+const Separator = () => (
+    <View style={styles.separator} />
+  );
+  
+
 const Calculator = () => {
-    const [value1, setValue1] = useState(null);
-    const [value2, setValue2] = useState(null);
-    const [result, setResult] = useState(null);
+    const [value1, setValue1] = useState(null), [value2, setValue2] = useState(null), [result, setResult] = useState(null);
+
     return (
         <View>
-            <Text>Podaj liczbę</Text>
+            <Text style = {styles.txt}>Podaj liczbę</Text>
             <NumericInput 
                 onChange={value => setValue1(value)} 
             />
-            <Text>Podaj liczbę</Text>
-            <NumericInput 
-                onChange={value => setValue2(value)} 
+            <Text style = {styles.txt}>Podaj liczbę</Text>
+            <NumericInput
+                onChange={value => setValue2(value)}
             />
+            <Separator/>
             <Button
                 title = "+"
                 onPress = { () => {
                     setResult(value1 + value2)
                 }}
             />
+            <Separator/>
             <Button
                 title = "-"
                 onPress = { () => {
                     setResult(value1 - value2)
                 }}
             />
+            <Separator/>
             <Button
                 title = "*"
                 onPress = { () => {
                     setResult(value1 * value2)
                 }}
             />
+            <Separator/>
             <Button
                 title = ":"
                 onPress = { () => {
                     setResult(value1 / value2)
                 }}
             />
-            <Text>{result}</Text>
+            <Text style = {styles.txt}>{result}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    txt: {
+        fontSize: 20,
+        marginVertical: 4,
+        textAlign: 'center'
+    },
+    separator: {
+        marginVertical: 2
+    }
+})
 
 export default Calculator;
